e0450a0903c8300f371df756645ce93e299d8b95
James Stuckey <jstuckey@rutgers.edu>
2018-08-01 12:18:28 -0400

    implementing linear search

--- src/App.js
+++ src/App.js
@@ -1,21 +1,46 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { search } from './search';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.output = 'something';
+    }
+
+    linearSubmit(e) {
+        e.preventDefault();
+        // console.log('linear search');
+    }
+
+    findInput(param) {
+        this.output = search(param);
+    }
+
+    render() {
+        return (
+            //   <div className="App">
+            //     <header className="App-header">
+            //       <img src={logo} className="App-logo" alt="logo" />
+            //       <h1 className="App-title">Welcome to React</h1>
+            //     </header>
+            //     <p className="App-intro">
+            //       To get started, edit <code>src/App.js</code> and save to reload.
+            //     </p>
+            //   </div>
+            <main>
+                <form onSubmit={e => this.linearSubmit(e)}>
+                    <label>Linear Search:</label>
+                    <input type="text" />
+                </form>
+                <form onSubmit={() => console.log('binary search')}>
+                    <label>Binary Search:</label>
+                    <input type="text" />
+                </form>
+            </main>
+        );
+    }
 }
 
 export default App;
